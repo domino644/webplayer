9fc603bf6dacdb214ec2c89e4a79bd9dce8f05bb
domino644 <cichemleko@gmail.com>
2021-03-26 13:43:28 +0100

    nom

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
-<template></template>
+<template>
+  <div>tu bedzie kiedys webplayer, tak za 2 dni jakos</div>
+</template>
 
 <script>
 export default {};
 </script>
 
 <style>
+:root {
+  font-family: "Lexend", sans-serif;
+}
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  font-family: inherit;
+  font-size: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 
 
-import App from './App01.vue'
+import App from './App.vue'
 
 
 import vuex from "./vuex"
