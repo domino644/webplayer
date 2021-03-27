63f57510cc2acf08d8cb70559cf261c7fbb761ba
domino644 <cichemleko@gmail.com>
2021-03-27 17:16:38 +0100

    nom

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,13 +1,12 @@
-//<template>
-  
-</template>
+<template></template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+img {
+  height: 150px;
+  width: 150px;
+}
 </style>
